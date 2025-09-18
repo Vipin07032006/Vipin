b79586c08f43590d6cc989df26095135471fda8c
Vipin07032006 <itzvipin06@gmail.com>
2025-09-18 10:22:17 +0530

    Create Zz.cpp
    It's my first c++ program.

---
+++ Zz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+
+{
+
+std::int x = 4;
+
+std::int y = 5;
+
+std::int z = 9;
+
+std::int sum = x + y + z;
+
+  
+std::cout << "x" << '\n';
+
+std::cout << "y" << '\n';
+
+std::cout << "z" << '\n';
+
+std::cout << "sum" << '\n';
+
+return 0;
+
+}
